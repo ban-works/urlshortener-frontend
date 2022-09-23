f50bdd405791b0f41434cf61cae6a1e5aaa57455
Alban Fresil <alban.fresil@gmail.com>
2022-09-23 16:59:11 +0200

    added suffix input

--- components/Form.js
+++ components/Form.js
@@ -3,41 +3,57 @@ import { useState, useEffect } from "react";
 
 export default function Form() {
   const [URL, setURL] = useState("");
+  const [suffix, setSuffix]= useState("");
   const [shortURL, setShortURL] = useState();
 
-  var response = shortURL;
+// var response = shortURL;
 
   async function handleSubmit() {
     console.log(URL);
     var newURL = await fetch(`https://b-z.fr/shortlink`, {
       method: "post",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ URL: URL }),
+      body: JSON.stringify({ URL: URL, suffix: suffix }),
     });
     newURL = await newURL.json();
     setShortURL(newURL);
   }
-  //   console.log(shortURL);
-  //   useEffect(() =>{
-  //     console.log(shortURL)
-  //     response = shortURL.URL
-  //   },[shortURL])
+    // useEffect(() =>{
+    //   response = shortURL
+    // },[shortURL])
 
   return (
     <Col>
-      <Row>
+      <Row justify="center">
         <Input
           onChange={(e) => setURL(e.target.value)}
           color="secondary"
           clearable
           placeholder="https://monurl.com/"
         />
+                </Row>
+                <Spacer x={0.5} />
+
+                <Row justify="center">
+
+        <Input
+          onChange={(e) => setSuffix(e.target.value)}
+          color="secondary"
+          clearable
+          placeholder="fin personnalisée (optionnel)"
+        />
+        
+        </Row>
         <Spacer x={0.5} />
+
+        <Row justify="center">
+
         <Button onPress={handleSubmit}>Créer mon lien B-Z.fr</Button>
       </Row>
-      <Row>
-      <Spacer y={0.5} />
-      <a href={response}>{response}</a>
+      <Spacer x={0.5} />
+
+      <Row justify="center">
+      <a href={shortURL}>{shortURL}</a>
 
       </Row>
     </Col>
